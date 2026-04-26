59d7e1738a2773c30016c135920d19cba85ee764
Parin Patil <parinvpatil@gmail.com>
2026-04-26 22:56:07 +0530

    Add header comment for SQL interview questions
    Added header comment with author and purpose information.

--- top_sql_questions.sql
+++ top_sql_questions.sql
@@ -1,1 +1,9 @@
+/* ==========================================================
+   TOP SQL INTERVIEW QUESTIONS
+-------------------------------------------------------------
+This file contains commonly asked SQL interview problems
+with example solutions.
 
+Author: Parin Patil
+Purpose: SQL practice and interview preparation
+========================================================== */
